59eacbd13d043952d4f82e636ffda676919fbf2a
HiranmayiKarri <139166164+HiranmayiKarri@users.noreply.github.com>
2023-08-16 14:12:30 +0530

    Create mainprog.cpp
    Spacecraft class which keeps track of the dimensions on x,y,z-axis and the direction.

---
+++ mainprog.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Spacecraft {
+private:
+    int x, y, z;
+    std::string direction;
+
+public:
+    Spacecraft(int x, int y, int z, std::string direction) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->direction = direction;
+    }
+};
+
+
+int main() {
+    int starting_x = 0, starting_y = 0, starting_z = 0;
+    std::string initial_direction = "N";
+    std::vector<std::string> commands = {"f", "r", "u", "b", "l"};
+
+    Spacecraft craft(starting_x, starting_y, starting_z, initial_direction);
+    craft.execute_commands(commands);
+    craft.print_final_state();
+
+    return 0;
+}
